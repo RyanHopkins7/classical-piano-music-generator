4ca9901a103a556fbd52268ec78180553f90f7c2
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-12-04 19:01:29 -0800

    Allow multiple files to be part of corpus

--- generate_midi.py
+++ generate_midi.py
@@ -1,12 +1,15 @@
 import mido
 from mido import Message, MidiFile, MidiTrack
 from markov_chain.markov_model import MarkovModel
+from os import listdir
 
-corpus_file = MidiFile('music/eflat_major/chp_op18.mid', clip=True)
-messages = (message for track in corpus_file.tracks for message in track)
+corpus_files = (MidiFile(f'music/eflat_major/{file_name}', clip=True) for file_name in listdir('music/eflat_major'))
+
+# lol 
+messages = (message for f in corpus_files for track in f.tracks[1:3] for message in track)
 
 # TODO: Make markov model init work with a generator not just a list
-mkv = MarkovModel(midi_track=list(messages), order=2)
+mkv = MarkovModel(midi_track=list(messages), order=3)
 
 # Type 0 single track file
 gen_midi = MidiFile(type=0)
